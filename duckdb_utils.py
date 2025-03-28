77ba38db0af471c44c054752eb4354f3e5193b3e
Kannan <g-kannan@outlook.com>
2025-03-28 14:45:24 +0530

    Add support for clickstream events and S3 integration; update database schema and utility functions

--- duckdb_utils.py
+++ duckdb_utils.py
@@ -1,5 +1,10 @@
 import duckdb
 import streamlit as st
+from dotenv import load_dotenv
+import os
+import uuid
+
+load_dotenv()
 
 def connect_duckdb():
     if 'conn' not in st.session_state:
@@ -8,3 +13,31 @@ def connect_duckdb():
 
 def create_tables(con):
     con.execute("create table IF NOT EXISTS page_visits(page_name TEXT, visit_ts datetime);")
+    con.execute("create table IF NOT EXISTS clickstream_events(event_type TEXT, visit_ts datetime);")
+    # con.execute("""CREATE TABLE IF NOT EXISTS orders (
+    #             id UUID PRIMARY KEY  DEFAULT  uuid(),
+    #             product VARCHAR,
+    #             quantity INTEGER,
+    #             mobile_number INTEGER,
+    #             delivery_location VARCHAR,
+    #             promo_code VARCHAR,
+    #             total_price DECIMAL(10, 2),
+    #             payment_option VARCHAR,
+    #             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
+    #             );
+    #         """)
+    
+def load_extentions_and_secrets(con):
+    con.execute("INSTALL httpfs;LOAD httpfs;")
+    con.execute(f"""
+                CREATE OR REPLACE SECRET (
+                TYPE s3,
+                KEY_ID '{os.getenv("AWS_ACCESS_KEY_ID")}',
+                SECRET '{os.getenv("AWS_SECRET_ACCESS_KEY")}',
+                REGION '{os.getenv("AWS_REGION")}'
+            );
+                """)
+
+def get_unique_filename():
+    unique_filename = f"{uuid.uuid4()}.parquet"
+    return unique_filename
--- home.py
+++ home.py
@@ -1,7 +1,14 @@
 import streamlit as st
-import duckdb
+from duckdb_utils import *
+
 
 st.header("Welcome to G-MART")
+con = connect_duckdb()
+create_tables(con)
+load_extentions_and_secrets(con)
+unique_filename = get_unique_filename()
+# con.execute("insert into page_visits values('/products', CURRENT_TIMESTAMP);")
+con.execute(f"copy (select '/home' as page_name,CURRENT_TIMESTAMP as visit_ts) to 's3://tgt-southdms/g-mart/page_visits/{unique_filename}'  (FORMAT parquet);")
 # user_name = st.text_input("Please enter your name")
 
-con = duckdb.connect(database=':memory:', read_only=False)
+# con = duckdb.connect(database=':memory:', read_only=False)
